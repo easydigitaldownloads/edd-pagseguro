15dd7a6246f1937a3737dc044cb23f0b0eb36edc
Pippin Williamson <pippin@pippinsplugins.com>
2015-06-11 09:01:48 -0500

    Updated API library

--- lib/PagSeguroLibrary/PagSeguroLibrary.php
+++ lib/PagSeguroLibrary/PagSeguroLibrary.php
@@ -1,85 +1,122 @@
 <?php
-/*
-************************************************************************
-Copyright [2011] [PagSeguro Internet Ltda.]
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-************************************************************************
-*/
-
-/*
- * PagSeguro Library Class
- * Version: 2.0.3
- * Date: 29/08/2011
+/**
+ * 2007-2014 [PagSeguro Internet Ltda.]
+ *
+ * NOTICE OF LICENSE
+ *
+ *Licensed under the Apache License, Version 2.0 (the "License");
+ *you may not use this file except in compliance with the License.
+ *You may obtain a copy of the License at
+ *
+ *http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *Unless required by applicable law or agreed to in writing, software
+ *distributed under the License is distributed on an "AS IS" BASIS,
+ *WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *See the License for the specific language governing permissions and
+ *limitations under the License.
+ *
+ *  @author    PagSeguro Internet Ltda.
+ *  @copyright 2007-2014 PagSeguro Internet Ltda.
+ *  @license   http://www.apache.org/licenses/LICENSE-2.0
  */
-define('PAGSEGURO_LIBRARY', TRUE);
-require_once "loader".DIRECTORY_SEPARATOR."PagSeguroAutoLoader.class.php";
-class PagSeguroLibrary {
 
-	const VERSION = "2.0.3";
-	private static $library;
-	private static $path;
-	public static $resources;
-	public static $config;
-	public static $log;
+class PagSeguroLibrary
+{
 
-	private function __construct() {
-		self::$path 	 = (dirname(__FILE__));
-		PagSeguroAutoloader::init();
-		self::$resources = PagSeguroResources::init();
-		self::$config 	 = PagSeguroConfig::init();
-		self::$log 	 	 = LogPagSeguro::init();
-	}
+    const VERSION = "2.5.0";
+    public static $resources;
+    public static $config;
+    public static $log;
+    private static $path;
+    private static $library;
+    private static $module_version;
+    private static $cms_version;
+    private static $php_version;
 
-	public static function init() {
-		self::verifyDependencies();
-		if (self::$library == null) {
-			self::$library  = new PagSeguroLibrary();
-		}
-		return self::$library;
-	}
+    private function __construct()
+    {
+        self::$path = (dirname(__FILE__));
+        PagSeguroAutoloader::init();
+        self::$resources = PagSeguroResources::init();
+        self::$config = PagSeguroConfig::init();
+        self::$log = LogPagSeguro::init();
+    }
 
-	private static function verifyDependencies() {
+    public static function init()
+    {
+        if (self::$library == null) {
+            require_once "loader" . DIRECTORY_SEPARATOR . "PagSeguroAutoLoader.class.php";
+            self::verifyDependencies();
+            self::$library = new PagSeguroLibrary();
+        }
+        return self::$library;
+    }
 
-		$dependencies = true;
+    private static function verifyDependencies()
+    {
 
-		if (!function_exists('spl_autoload_register')) {
-			throw new Exception("PagSeguroLibrary: Standard PHP Library (SPL) is required.");
-			$dependencies = false;
-		}
+        $dependencies = true;
 
-		if (!function_exists('curl_init')) {
-			throw new Exception('PagSeguroLibrary: cURL library is required.');
-			$dependencies = false;
-		}
+        try {
 
-		if (!class_exists('DOMDocument')) {
-			throw new Exception('PagSeguroLibrary: DOM XML extention is required.');
-			$dependencies = false;
-		}
+            if (!function_exists('curl_init')) {
+                $dependencies = false;
+                throw new Exception('PagSeguroLibrary: cURL library is required.');
+            }
 
-		return $dependencies;
+            if (!class_exists('DOMDocument')) {
+                $dependencies = false;
+                throw new Exception('PagSeguroLibrary: DOM XML extension is required.');
+            }
 
-	}
+        } catch (Exception $e) {
+            return $dependencies;
+        }
 
-	public final static function getVersion() {
-		return self::VERSION;
-	}
+        return $dependencies;
 
-	public final static function getPath() {
-		return self::$path;
-	}
+    }
 
+    final public static function getVersion()
+    {
+        return self::VERSION;
+    }
+
+    final public static function getPath()
+    {
+        return self::$path;
+    }
+
+    final public static function getModuleVersion()
+    {
+        return self::$module_version;
+    }
+
+    final public static function setModuleVersion($version)
+    {
+        self::$module_version = $version;
+    }
+
+    final public static function getPHPVersion()
+    {
+        return self::$php_version = phpversion();
+    }
+
+    final public static function setPHPVersion($version)
+    {
+        self::$php_version = $version;
+    }
+
+    final public static function getCMSVersion()
+    {
+        return self::$cms_version;
+    }
+
+    final public static function setCMSVersion($version)
+    {
+        self::$cms_version = $version;
+    }
 }
+
 PagSeguroLibrary::init();
-?>
--- lib/PagSeguroLibrary/service/PagSeguroNotificationService.class.php
+++ lib/PagSeguroLibrary/service/PagSeguroNotificationService.class.php
@@ -1,95 +1,182 @@
-<?php if (!defined('ALLOW_PAGSEGURO_CONFIG')) { die('No direct script access allowed'); }
-/*
-************************************************************************
-Copyright [2011] [PagSeguro Internet Ltda.]
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-************************************************************************
-*/
-
+<?php
 /**
+ * 2007-2014 [PagSeguro Internet Ltda.]
+ *
+ * NOTICE OF LICENSE
+ *
+ *Licensed under the Apache License, Version 2.0 (the "License");
+ *you may not use this file except in compliance with the License.
+ *You may obtain a copy of the License at
+ *
+ *http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *Unless required by applicable law or agreed to in writing, software
+ *distributed under the License is distributed on an "AS IS" BASIS,
+ *WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *See the License for the specific language governing permissions and
+ *limitations under the License.
+ *
+ *  @author    PagSeguro Internet Ltda.
+ *  @copyright 2007-2014 PagSeguro Internet Ltda.
+ *  @license   http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+/***
  * Encapsulates web service calls regarding PagSeguro notifications
  */
-class PagSeguroNotificationService {
-	
-	const serviceName = 'notificationService';
-	
-	private static function buildTransactionNotificationUrl(PagSeguroConnectionData $connectionData, $notificationCode) {
-		$url   = $connectionData->getServiceUrl();
-		return "{$url}/{$notificationCode}/?".$connectionData->getCredentialsUrlQuery();
-	}
-	
-	/**
-	 * Returns a transaction from a notification code
-	 * 
-	 * @param PagSeguroCredentials $credentials
-	 * @param String $notificationCode
-	 * @throws PagSeguroServiceException
-	 * @throws Exception
-	 * @return a transaction
-	 * @see PagSeguroTransaction
-	 */
-	public static function checkTransaction(PagSeguroCredentials $credentials, $notificationCode) {
-		
-		LogPagSeguro::info("PagSeguroNotificationService.CheckTransaction(notificationCode=$notificationCode) - begin");
-		$connectionData = new PagSeguroConnectionData($credentials, self::serviceName);
-		
-		try {
-			
-			$connection = new PagSeguroHttpConnection();
-			$connection->get(
-				self::buildTransactionNotificationUrl($connectionData, $notificationCode), // URL + parâmetros de busca
-				$connectionData->getServiceTimeout(), // Timeout
-				$connectionData->getCharset() // charset
-			);
-			
-			$httpStatus = new PagSeguroHttpStatus($connection->getStatus());
-			
-			switch ($httpStatus->getType()) {
-				
-				case 'OK':
-					// parses the transaction
-					$transaction = PagSeguroTransactionParser::readTransaction($connection->getResponse());
-					LogPagSeguro::info("PagSeguroNotificationService.CheckTransaction(notificationCode=$notificationCode) - end ". $transaction->toString().")");
-					break;
-				
-				case 'BAD_REQUEST':
-					$errors = PagSeguroTransactionParser::readErrors($connection->getResponse());
-					$e = new PagSeguroServiceException($httpStatus, $errors);
-					LogPagSeguro::info("PagSeguroNotificationService.CheckTransaction(notificationCode=$notificationCode) - error ".$e->getOneLineMessage());
-					throw $e;
-					break;
-					
-				default:
-					$e = new PagSeguroServiceException($httpStatus);
-					LogPagSeguro::info("PagSeguroNotificationService.CheckTransaction(notificationCode=$notificationCode) - error ".$e->getOneLineMessage());
-					throw $e;
-					break;
-					
-			}
-			
-			return isset($transaction) ? $transaction : null;
-			
-		} catch (PagSeguroServiceException $e) {
-			throw $e;
-		} catch (Exception $e) {
-			LogPagSeguro::error("Exception: ".$e->getMessage());
-			throw $e;
-		}
-		
-	}
+class PagSeguroNotificationService
+{
 
+    /***
+     *
+     */
+    const SERVICE_NAME = 'notificationService';
+
+    private static $logService;
+
+    /***
+     * @param PagSeguroConnectionData $connectionData
+     * @param $notificationCode
+     * @return string
+     */
+    private static function buildTransactionNotificationUrl(PagSeguroConnectionData $connectionData, $notificationCode)
+    {
+        $url = $connectionData->getServiceUrl();
+        return "{$url}/{$notificationCode}/?" . $connectionData->getCredentialsUrlQuery();
+    }
+
+    /***
+     * @param PagSeguroConnectionData $connectionData
+     * @param $notificationCode
+     * @return string
+     */
+    private static function buildAuthorizationNotificationUrl(PagSeguroConnectionData $connectionData, $notificationCode)
+    {
+        $url = $connectionData->getWebserviceUrl() . '/' . $connectionData->getResource('applicationPath');
+        return "{$url}/{$notificationCode}/?" . $connectionData->getCredentialsUrlQuery();
+    }
+
+    /***
+     * Returns a transaction from a notification code
+     *
+     * @param PagSeguroCredentials $credentials
+     * @param String $notificationCode
+     * @throws PagSeguroServiceException
+     * @throws Exception
+     * @return PagSeguroTransaction
+     * @see PagSeguroTransaction
+     */
+    public static function checkTransaction(PagSeguroCredentials $credentials, $notificationCode)
+    {
+
+        LogPagSeguro::info("PagSeguroNotificationService.CheckTransaction(notificationCode=$notificationCode) - begin");
+        $connectionData = new PagSeguroConnectionData($credentials, self::SERVICE_NAME);
+
+        try {
+
+            $connection = new PagSeguroHttpConnection();
+            $connection->get(
+                self::buildTransactionNotificationUrl($connectionData, $notificationCode),
+                $connectionData->getServiceTimeout(),
+                $connectionData->getCharset()
+            );
+
+            $transaction = PagSeguroTransactionParser::readTransaction($connection->getResponse());
+
+            self::$logService = "CheckTransaction";
+            return self::searchReturn($connection, $transaction, $notificationCode);
+
+        } catch (PagSeguroServiceException $err) {
+            throw $err;
+        } catch (Exception $err) {
+            LogPagSeguro::error("Exception: " . $err->getMessage());
+            throw $err;
+        }
+    }
+
+    /***
+     * Returns a authorization from a notification code
+     *
+     * @param PagSeguroCredentials $credentials
+     * @param String $notificationCode
+     * @throws PagSeguroServiceException
+     * @throws Exception
+     * @return PagSeguroAuthorization
+     * @see PagSeguroAuthorization
+     */
+    public static function checkAuthorization(PagSeguroCredentials $credentials, $notificationCode)
+    {
+
+        LogPagSeguro::info(
+            "PagSeguroNotificationService.CheckAuthorization(notificationCode=$notificationCode) - begin"
+        );
+
+        $connectionData = new PagSeguroConnectionData($credentials, self::SERVICE_NAME);
+
+        try {
+
+            $connection = new PagSeguroHttpConnection();
+            $connection->get(
+                self::buildAuthorizationNotificationUrl($connectionData, $notificationCode),
+                $connectionData->getServiceTimeout(),
+                $connectionData->getCharset()
+            );
+
+            $authorization = PagSeguroAuthorizationParser::readAuthorization($connection->getResponse());
+            self::$logService = "CheckAuthorization";
+            return self::searchReturn($connection, $authorization, $notificationCode);
+
+        } catch (PagSeguroServiceException $err) {
+            throw $err;
+        }
+        catch (Exception $err) {
+            LogPagSeguro::error("Exception: " . $err->getMessage());
+            throw $err;
+        }
+    }
+
+    /**
+     * @param PagSeguroHttpConnection $connection
+     * @param string $code
+     * @return bool|mixed|string
+     * @throws PagSeguroServiceException
+     */
+    private static function searchReturn($connection, $parsers, $code)
+    {
+        $httpStatus = new PagSeguroHttpStatus($connection->getStatus());
+
+        switch ($httpStatus->getType()) {
+
+            case 'OK':
+
+                LogPagSeguro::info(
+                    sprintf("PagSeguroNotificationService.%s(notificationCode=$code) - end ", self::$logService) .
+                    $parsers->toString() . ")"
+                );
+                break;
+
+            case 'BAD_REQUEST':
+
+                $errors = PagSeguroServiceParser::readErrors($connection->getResponse());
+
+                $err = new PagSeguroServiceException($httpStatus, $errors);
+                LogPagSeguro::info(
+                    sprintf("PagSeguroNotificationService.%s(notificationCode=$code) - error ", self::$logService) .
+                    $err->getOneLineMessage()
+                );
+                throw $err;
+                break;
+
+            default:
+                $err = new PagSeguroServiceException($httpStatus);
+                LogPagSeguro::info(
+                    sprintf("PagSeguroNotificationService.%s(notificationCode=$code) - error ", self::$logService) .
+                    $err->getOneLineMessage()
+                );
+                throw $err;
+                break;
+        }
+        return isset($parsers) ? $parsers : null;
+    }
 
 }
-	
-?>
